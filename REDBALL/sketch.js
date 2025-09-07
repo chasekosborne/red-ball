c49424e9ae9a7b8718e4a382a316c160030448a2
Zbush1 <zybush@gmail.com>
2025-09-06 22:09:07 -0400

    Set up the engine and template
    Got rid of the old template (sorry) and replaced it with one utilizing the q5.js and p5play its very plug and play Will add comments soon and explain how it works

---
+++ REDBALL/sketch.js
@@ -0,0 +1,29 @@
+new Q5();
+
+new Canvas();
+world.gravity.y = 10;
+
+let ball = new Sprite();
+ball.x = halfWidth - 200;
+ball.y = halfHeight - 200;
+ball.diameter = 50;
+
+let groundA = ground = new Sprite(500, 350, 800, 40);
+groundA.physics = STATIC;
+
+
+function update() {
+	background('skyblue');
+
+	textAlign(CENTER);
+	textSize(20);
+	text('click to jump!', halfWidth, halfHeight - 100);
+
+	if (kb.pressing('space') && ball.colliding(ground)) ball.vel.y = -5;
+	if (kb.pressing('left')) { ball.vel.x -= 0.5;        
+    if (ball.vel.x < -5) {ball.vel.x = -5;  }
+}
+	if (kb.pressing('right')) { ball.vel.x += 0.5;        
+    if (ball.vel.x > 5) {ball.vel.x = 5;  }
+}
+}
