8f851659bb5d0f70ce2a5c2713e104e8a76fb26b
Amitha Ajithkumar <amitha.ajithkumar4@gmail.com>
2025-12-01 23:25:29 -0500

    Added level 2
    level 2 changes

--- src/controller/teleporter.js
+++ src/controller/teleporter.js
@@ -1,32 +1,28 @@
 function teleportation() {
     if (!ball) return;
 
-    levelObjects.teleporter?.forEach(teleporter => {   //for each teleporter 
-        if (dist(ball.x, ball.y, teleporter.x, teleporter.y) < 45 && teleporterActive == true) {    //if ball is 45 pixes from teleporter and the teleporter is activated
-                if (teleporter === levelObjects.teleporter[0]) {
-                    ball.x = levelObjects.teleporter[1].x;  //changes ball position to other teleporter
-                    ball.y = levelObjects.teleporter[1].y;
+    levelObjects.teleporter?.forEach((teleporter, index) => {  
+        if (dist(ball.x, ball.y, teleporter.x, teleporter.y) < 45 && teleporterActive == true) {   
+                let pairIndex;
+                if (index % 2 === 0) {
+                    pairIndex = index + 1;
+                } else {
+                    pairIndex = index - 1;
+                }
+                if (pairIndex >= 0 && pairIndex < levelObjects.teleporter.length) {
+                    ball.x = levelObjects.teleporter[pairIndex].x;  
+                    ball.y = levelObjects.teleporter[pairIndex].y;
                     if(teleportSound && teleportSound.isLoaded()) {
                       teleportSound.setVolume(globalVolume * 0.25);
                       teleportSound.play(); 
                     } 
 
-                    teleporterActive = false;     //deactivates teleporter temporarily
-                    beginTime = millis();         //logs the milliseconds when teleportation occured
-                } else if (teleporter === levelObjects.teleporter[1] && teleporterActive == true) {
-                    ball.x = levelObjects.teleporter[0].x;        
-                    ball.y = levelObjects.teleporter[0].y;
-                    if(teleportSound && teleportSound.isLoaded()) {
-                      teleportSound.setVolume(globalVolume * 0.25);
-                      teleportSound.play(); 
-                    } 
-                    teleporterActive = false;
-                    beginTime = millis();
-                   
-            } 
-    }
-        if(millis() - beginTime >=3000){  //after a 3 second delay, teleporter can be used again
-            teleporterActive = true;     //activates teleporter
+                    teleporterActive = false;     
+                    beginTime = millis();         
+                }
+        }
+        if(millis() - beginTime >=3000){  
+            teleporterActive = true;    
         }
 
     }); 
